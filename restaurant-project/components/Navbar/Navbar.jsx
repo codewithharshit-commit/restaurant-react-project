8dda46a1176027ee9cdaa25f81dff91de4bd4098
codewithharshit-commit <sharma.harshit.0008@gmail.com>
2026-01-27 18:04:58 +0530

    started react restaurant project

---
+++ restaurant-project/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {GiHamburgerMenu} from 'react-icons/gi'
+import './Navbar.css';
+import "../../src/index.css"
+
+const Navbar = () => (
+  <nav className='border-2'>
+    Navbar
+  </nav>
+);
+
+export default Navbar;
---
+++ restaurant-project/src/App.jsx
@@ -0,0 +1,13 @@
+import {Navbar} from "../components"
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
+
+// https://wordpress.iqonic.design/product/wp-free/geritcht/wp-content/themes/geritcht-theme/assets/images/redux/loader.gif
